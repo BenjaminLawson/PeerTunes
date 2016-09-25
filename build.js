a4bfe9d35005f16d1677b6276c4b3e8493c25c36
Ben <blinternetgeek@gmail.com>
2016-09-25 01:37:37 -0700

    build dist with uglify

--- build.js
+++ build.js
@@ -1,5 +1,5 @@
 //build PeerTunes for static file distribution
-//TODO: minify css, js
+//TODO: minify
 
 var fs = require('fs')
 var path = require('path')
@@ -7,6 +7,7 @@ var ejs = require('ejs')
 var ncp = require('ncp').ncp
 var browserify = require('browserify')
 var exorcist = require('exorcist')
+var UglifyJS = require("uglify-js")
 
 var views = [
 	{
@@ -36,34 +37,17 @@ ncp('./public', './dist', function (err) {
 
 //browserify app scripts
 function browserifyApp () {
-	var mapFile = path.join(__dirname, './dist/js/app.js.map')
-	/*
-	var options = {
-		map: '/js/app.js.map',
-		include: ['src/*','src/**'],
-		exclude: ['node_modules/jsmediatags/**','node_modules/jsmediatags/build2/*.js'],
-		output: mapFile //source map
-	}
+	//var mapFile = path.join(__dirname, './dist/js/app.js.map')
 
-	//{debug: false}
-	var bundler = browserify({ debug: true })
-	bundler.add('./src/main.js')
-	bundler.plugin('minifyify', {map: '/js/app.js.map'})
-	bundler.bundle(function (err, src, map) {
-	  // Your code here
-	  console.log('Done bundling')
-
-	})
-	*/
-	//.pipe(exorcist(mapFile))
-	var appFile = fs.createWriteStream('dist/js/app.js', 'utf8')
-	appFile.on('open', function(fd) {
-		browserify({debug: true})
-	  .add('src/main.js')
+	var bundler = browserify('src/main.js')
+	bundler.transform({ 
+		global: true, 
+		ignore: [
+    , '**/node_modules/jsmediatags/**'
+  	],
+		sourcemap: false }, 'uglifyify')
 	  .bundle()
-	  .pipe(exorcist(mapFile))
-	  .pipe(appFile)
-	})
+	  .pipe(fs.createWriteStream('dist/js/app.js'))
 }
 
 
